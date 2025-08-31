b6738acdee80889f1c1fd22c5a164b4bab036a93
kulshresth5 <kulsingh1701@gmail.com>
2025-08-31 18:33:40 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.json()); 
 app.use(cookieParser());
 const corsOption={
-    origin:'http://localhost:3000',
+    origin:'https://chat-app-1-ptr4.onrender.com',
     credentials:true
 };
 app.use(cors(corsOption)); 
